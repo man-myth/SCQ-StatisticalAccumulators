2b3dfdf67d726837717da3fa6b7e6c031b53a50f
matthew <2213277@slu.edu.ph>
2023-03-20 23:01:55 +0800

    initial commit

---
+++ src/ManufacturingSystem.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class ManufacturingSystem {
+    public static void main(String[] args) {
+        HashMap<Integer, Double[]> machineParts = new HashMap<>();
+        machineParts.put(1, new Double[]{0.0,1.73,2.90});  // These are given
+    }
+}
