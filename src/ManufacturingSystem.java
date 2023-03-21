5108f92c7d9d03cc4c955e6251415bf21f944ffd
manmyth <manmeets1100@gmail.com>
2023-03-21 14:27:24 +0800

    add doNextEvent

--- src/ManufacturingSystem.java
+++ src/ManufacturingSystem.java
@@ -7,9 +7,22 @@ import java.util.Stack;
 public class ManufacturingSystem {
     HashMap<Integer, Double[]> machineParts = new HashMap<>();
     ArrayList<Object> simulation = new ArrayList<>();
+    int entityno = 0;
+    double time  = 0;
+    String eventType = "Init";
+    int qt = 0;
+    boolean bt = false;
+    Stack<Double> queue = new Stack<>();
+    double inService  = 0;
+    int p = 0;
+    int n = 0;
+    double wqsum = 0;
+    double wqmax = 0;
+    double tssum = 0;
+    double tsmax = 0;
 
     public ManufacturingSystem(){
-        machineParts.put(1, new Double[]{0.0, 1.73, 2.90});
+        machineParts.put(1, new Double[]{0.0, 1.73, 2.90}); // cust no., arrival time, interarrival time, service time
         machineParts.put(2, new Double[]{1.73, 1.35, 1.76});
         machineParts.put(3, new Double[]{3.08, 0.71, 3.39});
         machineParts.put(4, new Double[]{3.79, 0.62, 4.52});
@@ -20,25 +33,23 @@ public class ManufacturingSystem {
         machineParts.put(9, new Double[]{38.06, 1.76, 2.37});
         machineParts.put(10, new Double[]{39.82, 1.00, 5.38});
         machineParts.put(11, new Double[]{40.82, null, null});
-
-        double time  = 0;
-        String eventType = "Init";
-        int qt = 0;
-        boolean bt = false;
-        Stack<Double> queue = new Stack<>();
-        double inService  = 0;
-        int p = 0;
-        int n = 0;
-        double wqsum = 0;
-        double wqmax = 0;
-        double tssum = 0;
-        double tsmax = 0;
-        simulation.add(List.of(time,eventType,qt,bt,queue,inService,p,n,wqsum,wqmax,tssum,tsmax));
+        simulation.add(List.of(entityno,time,eventType,qt,bt,queue,inService,p,n,wqsum,wqmax,tssum,tsmax));
     }
 
     public void doNextEvent(){
+        entityno ++;
+        Double[] attributes = machineParts.get(entityno);
+        time = attributes[0];
+        queue.add(time);
+
+
+
 
     }
 
+    public static void main(String[] arg){
+        ManufacturingSystem mn = new ManufacturingSystem();
+
+    }
 
 }
