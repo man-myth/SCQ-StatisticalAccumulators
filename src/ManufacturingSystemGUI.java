7c8f4d7f82549c0d9d0e9072576c8cf71c0ac27f
manmyth <manmeets1100@gmail.com>
2023-03-26 22:43:50 +0800

    Add averages in accumulators

--- src/ManufacturingSystemGUI.java
+++ src/ManufacturingSystemGUI.java
@@ -35,6 +35,11 @@ public class ManufacturingSystemGUI extends JFrame {
     private JTextField input;
     private JLabel ausLabel;
     private JLabel ausValue;
+    private JLabel awtValue;
+    private JLabel awtLabel;
+    private JLabel atsValue;
+    private JLabel aqtValue;
+    private JLabel udpValue;
 
     // Defining headers for the table
     String [] headers = {"Entity No.", "Time", "Event type", "Q(t)",
@@ -75,67 +80,81 @@ public class ManufacturingSystemGUI extends JFrame {
                 // Getting User input
                 maxTime = Integer.parseInt(input.getText());
 
-                if(maxTime>40) {
+                if (maxTime > 40) {
                     JOptionPane.showMessageDialog(null, "Invalid input, it must be lower than 40");
                 } else {
                     accept = true;
                 }
 
-                if(accept) {
+                if (accept) {
                     // Creating a new ManufacturingSystem object
                     ManufacturingSystem ms = new ManufacturingSystem();
                     ArrayList<ArrayList<Object>> rows = new ArrayList<ArrayList<Object>>();
                     rows.add(ms.simulation);
-
-                for(double time = ms.time; time < maxTime;){
-                    try {
-                        ArrayList<Object> row = new ArrayList<Object>(ms.simulation);
-                        row.set(5, row.get(5).toString());
-                        rows.add(row);
-                        ms.goToNextEvent();
-                        time = ms.time;
-                    }catch (Exception ex){
-                        System.out.println(ex);
-                        break;
+                    for (double time = ms.time; time < maxTime; ) {
+                        try {
+                            ArrayList<Object> row = new ArrayList<Object>(ms.simulation);
+                            row.set(5, row.get(5).toString());
+                            rows.add(row);
+                            ms.goToNextEvent();
+                            time = ms.time;
+                        } catch (Exception ex) {
+                            System.out.println(ex);
+                            break;
+                        }
                     }
-                }
-
-                //put the first element at the end
-                ArrayList<Object> firstElement = rows.remove(0);
-                    if (Double.parseDouble(firstElement.get(1).toString()) < maxTime) {
-                        rows.add(firstElement);
-                    }
+                    //put the first element at the end
+                    ArrayList<Object> firstElement = rows.remove(0);
+                    rows.add(firstElement);
+//                    if (Double.parseDouble(firstElement.get(1).toString()) < maxTime) {
+//                        rows.add(firstElement);
+//                    }
                     //converting arraylist to array
                     results = rows.stream().map(u -> u.toArray(new Object[0])).toArray(Object[][]::new);
 
-                if(((Number) results[results.length-1][1]).doubleValue() > maxTime){
-                    results[results.length-1][0] = " ";
-                    results[results.length-1][1] = maxTime;
-                    results[results.length-1][2] = "End";
+                    if (((Number) results[results.length - 1][1]).doubleValue() > maxTime) {
+                        results[results.length - 1][0] = " ";
+                        results[results.length - 1][1] = maxTime;
+                        results[results.length - 1][2] = "End";
+                        results[results.length - 1][3] = results[results.length - 2][3];
+                        results[results.length - 1][4] = results[results.length - 2][4];
+                        results[results.length - 1][5] = results[results.length - 2][5];
+                        results[results.length - 1][6] = results[results.length - 2][6];
+                        results[results.length - 1][7] = results[results.length - 2][7];
+                        results[results.length - 1][8] = results[results.length - 2][8];
+                        results[results.length - 1][9] = results[results.length - 2][9];
+                        results[results.length - 1][10] = results[results.length - 2][10];
+                        results[results.length - 1][11] = results[results.length - 2][11];
+                        results[results.length - 1][12] = results[results.length - 2][12];
+                    }
+
+                    // Updating the table with the new results
+                    table.setModel(new DefaultTableModel(
+                            results,
+                            headers
+                    ));
+
+                    double qtSum = computeAreaUnderCurve(results, 'q');
+                    double btSum = computeAreaUnderCurve(results, 'b');
+
+                    // Updating the analytics panel with the new values
+                    nppValue.setText((results[results.length - 1][7])+ " ") ;
+                    ltsValue.setText((results[results.length - 1][10]) + " mins");
+                    twtValue.setText(String.valueOf(results[results.length - 1][9]));
+                    awtValue.setText((((Number)results[results.length - 1][9]).doubleValue() / ((Number)results[results.length - 1][8]).doubleValue())+ " mins/part");
+                    nptValue.setText(String.valueOf(results[results.length - 1][8]));
+                    ltValue.setText(String.valueOf(results[results.length - 1][12]));
+                    atsValue.setText((((Number)results[results.length - 1][11]).doubleValue() / ((Number)results[results.length - 1][7]).doubleValue())+ " mins/part");
+                    auqvalue.setText(String.valueOf(qtSum));
+                    htqValue.setText(getMax(results));
+                    aqtValue.setText(qtSum / maxTime + " part");
+                    ausValue.setText(String.valueOf(btSum));
+                    udpValue.setText(btSum/maxTime * 100 + "%");
                 }
 
-                // Updating the table with the new results
-                table.setModel(new DefaultTableModel(
-                        results,
-                        headers
-                ));
-
-                double qtSum = computeAreaUnderCurve(results,'q');
-                double btSum = computeAreaUnderCurve(results,'b');
-
-                // Updating the analytics panel with the new values
-                nppValue.setText(String.valueOf(results[results.length-1][7]));
-                twtValue.setText(String.valueOf(results[results.length-1][9]));
-                nptValue.setText(String.valueOf(results[results.length-1][8]));
-                ltsValue.setText(String.valueOf(results[results.length-1][10]));
-                ltValue.setText(String.valueOf(results[results.length-1][12]));
-                auqvalue.setText(String.valueOf(qtSum));
-                htqValue.setText(getMax(results));
-                ausValue.setText(String.valueOf(btSum));
             }
-
-
-        });
+        }
+        );
     }
     // Start the queuing simulation
     public static void main(String[] args) {
@@ -145,7 +164,7 @@ public class ManufacturingSystemGUI extends JFrame {
     public double computeAreaUnderCurve(Object[][] table, char cat){
         System.out.println("For " + cat);
         double sum = 0;
-        for(int i = 2; i <table.length-1;i++){
+        for(int i = 2; i <table.length;i++){
             if(cat == 'q'){
 //                System.out.println("time = "+((Number) table[i][1]).doubleValue());
 //                System.out.println("time prev = "+((Number) table[i-1][1]).doubleValue());
