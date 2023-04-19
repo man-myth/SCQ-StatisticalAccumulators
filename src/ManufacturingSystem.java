4e509ef45f79bab19f6dd1516f668016f6ae10d4
manmyth <manmeets1100@gmail.com>
2023-04-19 17:00:42 +0800

    remove time limit on simulation

--- src/ManufacturingSystem.java
+++ src/ManufacturingSystem.java
@@ -1,28 +1,27 @@
-
 import java.util.*;
 
 public class ManufacturingSystem {
-     HashMap<Integer, Double[]> machineParts = new HashMap<>();
-     ArrayList<Object> simulation = new ArrayList<>();
-     int entityno = 0;
-     double time  = 0;
-     String eventType = "Init";
-     int qt = 0;
-     int bt = 0;
-     Queue<Double> queue = new LinkedList<>();
-     double inService  = 0;
-     int p = 0;
-     int n = 0;
-     double wqsum = 0;
-     double wqmax = 0;
-     double tssum = 0;
-     double tsmax = 0;
-     double sq = 0;
-     double sqmax = 0;
-     double sb = 0;
+    HashMap<Integer, Double[]> machineParts = new HashMap<>();
+    ArrayList<Object> simulation = new ArrayList<>();
+    int entityno = 0;
+    double time = 0;
+    String eventType = "Init";
+    int qt = 0;
+    int bt = 0;
+    Queue<Double> queue = new LinkedList<>();
+    double inService = 0;
+    int p = 0;
+    int n = 0;
+    double wqsum = 0;
+    double wqmax = 0;
+    double tssum = 0;
+    double tsmax = 0;
+    double sq = 0;
+    double sqmax = 0;
+    double sb = 0;
 
     //instantiator
-    public ManufacturingSystem(){
+    public ManufacturingSystem() {
         machineParts.put(1, new Double[]{0.0, 2.90, 0.0}); // Key: custno, Value: [arrival time, service time, start of service]
         machineParts.put(2, new Double[]{1.73, 1.76, 0.0});
         machineParts.put(3, new Double[]{3.08, 3.39, 0.0});
@@ -37,12 +36,9 @@ public class ManufacturingSystem {
         updateSimulationrow();
     }
 
-    public void goToNextEvent(){
-        if(getKey(inService) == machineParts.size()-1){
-            generateNewEntity(); // generate a new entity if there is no part next in the queue
-        }
+    public void goToNextEvent() {
         // check if the next entity in the list is in the queue
-        if(queue.contains(machineParts.get(entityno+1)[0]) & eventType == "Dep") {
+        if (queue.contains(machineParts.get(entityno + 1)[0]) & eventType == "Dep") {
             ++entityno;
             entityno = getKey(inService);
             eventType = "Dep";
@@ -53,48 +49,48 @@ public class ManufacturingSystem {
             inService = queue.remove();
             double arr = machineParts.get(getKey(inService))[0];
             double ser = machineParts.get(getKey(inService))[1];
-            machineParts.put(getKey(inService),new Double[]{arr,ser,time});
+            machineParts.put(getKey(inService), new Double[]{arr, ser, time});
             bt = 1;
             n++;
             p++;
             qt = queue.size();
-            if(qt > sqmax){
+            if (qt > sqmax) {
                 sqmax = qt;
             }
-            wqmax = (Math.round((time - inService)*100.0)/100.0);
-            wqsum = (Math.round((wqmax + wqsum)*100.0)/100.0);
-            if(tsmax < Math.round(((inService + wqsum) - tssum)*100.0)/100.0)
-                tsmax = Math.round(((inService + wqsum) - tssum)*100.0)/100.0;
+            wqmax = (Math.round((time - inService) * 100.0) / 100.0);
+            wqsum = (Math.round((wqmax + wqsum) * 100.0) / 100.0);
+            if (tsmax < Math.round(((inService + wqsum) - tssum) * 100.0) / 100.0)
+                tsmax = Math.round(((inService + wqsum) - tssum) * 100.0) / 100.0;
             tssum = inService + wqsum;
             updateSimulationrow();
             entityno = getKey(inService);
 
             return;
         }
-        try{
+        try {
             //Check if the person in queue is done
-            if(machineParts.get(getKey(inService))[2] + machineParts.get(getKey(inService))[1] <= machineParts.get(entityno+1)[0]){
+            if (machineParts.get(getKey(inService))[2] + machineParts.get(getKey(inService))[1] <= machineParts.get(entityno + 1)[0]) {
                 entityno = getKey(inService);
                 eventType = "Dep";
                 double tempTime = time;
                 time = machineParts.get(getKey(inService))[2] + machineParts.get(getKey(inService))[1];
                 // compute for integrals
                 computeIntegral(tempTime);
-                if(!queue.isEmpty()){
+                if (!queue.isEmpty()) {
                     inService = queue.remove();
                     double arr = machineParts.get(getKey(inService))[0];
                     double ser = machineParts.get(getKey(inService))[1];
-                    machineParts.put(getKey(inService),new Double[]{arr,ser,time});
+                    machineParts.put(getKey(inService), new Double[]{arr, ser, time});
                     bt = 1;
                     n++;
                     p++;
                     qt = queue.size();
-                    if(qt > sqmax){
+                    if (qt > sqmax) {
                         sqmax = qt;
                     }
-                    wqmax = Math.max((Math.round((time - inService)*100.0)/100.0),wqmax);
-                    wqsum = ((((Math.round((time - inService)*100.0)/100.0) + wqsum)* 100.0) / 100.0);
-                    if(tsmax < Math.round(((inService + wqsum) - tssum)*100.0)/100.0) {
+                    wqmax = Math.max((Math.round((time - inService) * 100.0) / 100.0), wqmax);
+                    wqsum = ((((Math.round((time - inService) * 100.0) / 100.0) + wqsum) * 100.0) / 100.0);
+                    if (tsmax < Math.round(((inService + wqsum) - tssum) * 100.0) / 100.0) {
                         tsmax = Math.round(((inService + wqsum) - tssum) * 100.0) / 100.0;
                     }
                     tssum = inService + wqsum;
@@ -106,10 +102,10 @@ public class ManufacturingSystem {
                     p++;
                     bt = 0;
                     qt = queue.size();
-                    if(qt > sqmax){
+                    if (qt > sqmax) {
                         sqmax = qt;
                     }
-                    if (tsmax < Math.round(((time + wqsum) - tssum)*100.0)/100.0) {
+                    if (tsmax < Math.round(((time + wqsum) - tssum) * 100.0) / 100.0) {
                         tsmax = Math.round(((time + wqsum) - tssum) * 100.0) / 100.0;
                     }
 
@@ -120,35 +116,33 @@ public class ManufacturingSystem {
                 }
                 return;
             }
-        } catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println(ex);
         }
 
         // Check for arrivals
-        if(time <= machineParts.get(entityno+1)[0]){
+        if (time <= machineParts.get(entityno + 1)[0]) {
             double tempTime = time;
             entityno += 1;
-//            if(entityno < machineParts.size()){
-//                generateNewEntity();
-//            }
+
             Double[] attributes = machineParts.get(entityno);
             time = attributes[0];
             // compute for integrals
             computeIntegral(tempTime);
-            if(bt == 0){
+            if (bt == 0) {
                 inService = machineParts.get(entityno)[0];
                 bt = 1;
                 n++;
                 double arr = machineParts.get(getKey(inService))[0];
                 double ser = machineParts.get(getKey(inService))[1];
-                machineParts.put(getKey(inService),new Double[]{arr,ser,time});
+                machineParts.put(getKey(inService), new Double[]{arr, ser, time});
             } else {
                 queue.add(machineParts.get(entityno)[0]);
             }
 
             eventType = "Arr";
             qt = queue.size();
-            if(qt > sqmax){
+            if (qt > sqmax) {
                 sqmax = qt;
             }
             for (Map.Entry<Integer, Double[]> entry : machineParts.entrySet()) {
@@ -163,7 +157,8 @@ public class ManufacturingSystem {
         }
         updateSimulationrow();
     }
-    public void updateSimulationrow(){
+
+    public void updateSimulationrow() {
         simulation.clear();
         simulation.add(entityno);
         simulation.add(time);
@@ -184,67 +179,68 @@ public class ManufacturingSystem {
     }
 
 
-    public  int getKey(double value){
-        for (Map.Entry<Integer, Double[]> set :machineParts.entrySet()) {
-            if(set.getValue()[0] == value){
+    public int getKey(double value) {
+        for (Map.Entry<Integer, Double[]> set : machineParts.entrySet()) {
+            if (set.getValue()[0] == value) {
                 return set.getKey();
             }
         }
         return 0;
     }
 
-    public void computeIntegral(double prevTime){
-        sq += (time-prevTime) * qt;
-        sb += (time-prevTime) * bt;
+    public void computeIntegral(double prevTime) {
+        sq += Math.round(((time - prevTime) * qt) * 100.0) / 100.0;
+        sb += Math.round(((time - prevTime) * bt) * 100.0) / 100.0;
 
     }
 
-    public void generateNewEntity(){
+    public void generateNewEntity() {
         //generate arrival time
         double interarrivalTime = generateInterArrivalTime();
-        double arrivalTime = Math.round((interarrivalTime +  machineParts.get(machineParts.size())[0]) * 100.0) / 100.0; //add interarrival time to arrival time of previous part.
+        double arrivalTime = Math.round((interarrivalTime + machineParts.get(machineParts.size())[0]) * 100.0) / 100.0; //add interarrival time to arrival time of previous part.
         double serviceTime = Math.round(generateServiceTime() * 100.0) / 100.0;
-        System.out.println("Adding entity no " + (machineParts.size()+1) + "with interarrivaltime " + interarrivalTime + " prev time " + machineParts.get(machineParts.size())[0] + " service time " + serviceTime);
-        machineParts.put(machineParts.size()+1, new Double[]{arrivalTime, serviceTime, 0.0});
+        System.out.println("Adding entity no " + (machineParts.size() + 1) + "with interarrivaltime " + interarrivalTime + " prev time " + machineParts.get(machineParts.size())[0] + " service time " + serviceTime);
+        machineParts.put(machineParts.size() + 1, new Double[]{arrivalTime, serviceTime, 0.0});
 
     }
 
-    public double generateServiceTime(){
-        Random r  = new Random();
+    public double generateServiceTime() {
+        Random r = new Random();
         int randInt = r.nextInt(100);
         double[][] serviceTimeProbabilities = {         //cumulative probability, minvalue, maxvalue
-                {9,1,1.99},
-                {39,2,2.99},
-                {59,3,3.99},
-                {89,4,4.99},
-                {99,5,5.99}
+                {9, 1, 1.99},
+                {39, 2, 2.99},
+                {59, 3, 3.99},
+                {89, 4, 4.99},
+                {99, 5, 5.99}
         };
         double serviceTime = 0;
-        for(int i = 0; i<serviceTimeProbabilities.length;i++){
-            if(randInt<serviceTimeProbabilities[i][0]){
+        for (int i = 0; i < serviceTimeProbabilities.length; i++) {
+            if (randInt < serviceTimeProbabilities[i][0]) {
                 double rangeMin = serviceTimeProbabilities[i][1];
                 double rangeMax = serviceTimeProbabilities[i][2];
-                serviceTime = (rangeMin + (rangeMax - rangeMin) * r.nextDouble()) ;
+                serviceTime = (rangeMin + (rangeMax - rangeMin) * r.nextDouble());
                 break;
             }
         }
 
         return serviceTime;
     }
-    public double generateInterArrivalTime(){
-        Random r  = new Random();
+
+    public double generateInterArrivalTime() {
+        Random r = new Random();
         int randInt = r.nextInt(100);
-        double[][] interArrivalProbabilities ={
-                {29,0.1,0.99},
-                {69,1,1.99},
-                {79,3,3.99},
-                {89,14,14.99},
-                {99,15,15.99}
+        double[][] interArrivalProbabilities = {
+                {29, 0.1, 0.99},
+                {69, 1, 1.99},
+                {79, 3, 3.99},
+                {89, 14, 14.99},
+                {99, 15, 15.99}
         };
         double arrivalTime = 0;
 
-        for(int i = 0; i<interArrivalProbabilities.length; i++){
-            if(randInt<interArrivalProbabilities[i][0]){
+        for (int i = 0; i < interArrivalProbabilities.length; i++) {
+            if (randInt < interArrivalProbabilities[i][0]) {
                 double rangeMin = interArrivalProbabilities[i][1];
                 double rangeMax = interArrivalProbabilities[i][2];
                 arrivalTime = (rangeMin + (rangeMax - rangeMin) * r.nextDouble());
--- src/ManufacturingSystemGUI.java
+++ src/ManufacturingSystemGUI.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import javax.swing.table.DefaultTableModel;
@@ -10,6 +9,13 @@ import java.util.ArrayList;
 
 
 public class ManufacturingSystemGUI extends JFrame {
+    // Defining headers for the table
+    String[] headers = {"Entity No.", "Time", "Event type", "Q(t)",
+            "B(t)", "In Queue", "In service", "P", "N", "ΣWQ", "WQ*", "ΣTQ", "TS*", "∫Q", "∫Q*", "∫B"};
+    // Initializing the results array
+    Object[][] results = new Object[1][1];
+    // Initializing variables to keep track of the maximum time and customer values
+    double maxTime = 0;
     // Declaring components of the GUI
     private JPanel mainPanel;
     private JLabel title;
@@ -17,7 +23,6 @@ public class ManufacturingSystemGUI extends JFrame {
     private JPanel rigthPanel;
     private JButton simulateButton;
     private JTable table;
-
     // Declaring components of the analytics panel
     private JPanel AnalysisPanel;
     private JLabel nppLabel;
@@ -27,7 +32,6 @@ public class ManufacturingSystemGUI extends JFrame {
     private JLabel ltiLabel;
     private JLabel auqLabel;
     private JLabel hlqLabel;
-
     private JLabel nppValue;
     private JLabel twtValue;
     private JLabel nptValue;
@@ -47,18 +51,8 @@ public class ManufacturingSystemGUI extends JFrame {
     private JPanel leftPanel;
     private JScrollPane scrollPane;
 
-    // Defining headers for the table
-    String [] headers = {"Entity No.", "Time", "Event type", "Q(t)",
-            "B(t)", "In Queue", "In service", "P", "N", "ΣWQ", "WQ*", "ΣTQ", "TS*", "∫Q", "∫Q*", "∫B"};
-
-    // Initializing the results array
-    Object[][] results = new Object[1][1];
-
-    // Initializing variables to keep track of the maximum time and customer values
-    double maxTime = 0;
-
     // Constructor for the QueuingSystemGUI class
-    public ManufacturingSystemGUI(){
+    public ManufacturingSystemGUI() {
         // Calling the constructor of the JFrame superclass and setting properties of the GUI
         super("midterm-act2");
         Border raisedEtched = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
@@ -88,129 +82,120 @@ public class ManufacturingSystemGUI extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 // Resetting the maxTime and maxCustomer variables
                 maxTime = 0;
-                boolean accept = true;
 
                 // Getting User input
                 maxTime = Double.parseDouble(input.getText());
-
-//                if (maxTime > 39) {
-////                    JOptionPane.showMessageDialog(null, "Invalid input, it must be lower than 40");
-//                    accept = true;
-//                } else {
-//                    accept = true;
-//                }
-
-                if (accept) {
-                    // Creating a new ManufacturingSystem object
-                    ManufacturingSystem ms = new ManufacturingSystem();
-                    ArrayList<ArrayList<Object>> rows = new ArrayList<ArrayList<Object>>();
-                    rows.add(ms.simulation);
-                    for (double time = ms.time; time < maxTime; ) {
-                        try {
-                            ArrayList<Object> row = new ArrayList<Object>(ms.simulation);
-                            row.set(5, row.get(5).toString());
-                            rows.add(row);
-                            ms.goToNextEvent();
-                            time = ms.time;
-                        } catch (Exception ex) {
-                            break;
-                        }
+                // Creating a new ManufacturingSystem object
+                ManufacturingSystem ms = new ManufacturingSystem();
+                ArrayList<ArrayList<Object>> rows = new ArrayList<ArrayList<Object>>();
+                rows.add(ms.simulation);
+                for (double time = ms.time; time < maxTime; ) {
+                    if (ms.getKey(ms.inService) == ms.machineParts.size() - 2) {
+                        ms.generateNewEntity(); // generate a new entity if there is no part next in the queue
                     }
-                    //put the first element at the end
-                    ArrayList<Object> firstElement = rows.remove(0);
-                    rows.add(firstElement);
-//                    if (Double.parseDouble(firstElement.get(1).toString()) < maxTime) {
-//                        rows.add(firstElement);
-//                    }
-                    //converting arraylist to array
+                    try {
+                        ArrayList<Object> row = new ArrayList<Object>(ms.simulation);
+                        row.set(5, row.get(5).toString());
+                        rows.add(row);
+                        ms.goToNextEvent();
+                        time = ms.time;
 
-                    results = rows.stream().map(u -> u.toArray(new Object[0])).toArray(Object[][]::new);
-
-                    if (((Number) results[results.length - 1][1]).doubleValue() > maxTime) {
-                        results[results.length - 1][0] = " ";
-                        results[results.length - 1][1] = maxTime;
-                        results[results.length - 1][2] = "End";
-                        results[results.length - 1][3] = results[results.length - 2][3];
-                        results[results.length - 1][4] = results[results.length - 2][4];
-                        results[results.length - 1][5] = results[results.length - 2][5];
-                        results[results.length - 1][6] = results[results.length - 2][6];
-                        results[results.length - 1][7] = results[results.length - 2][7];
-                        results[results.length - 1][8] = results[results.length - 2][8];
-                        results[results.length - 1][9] = results[results.length - 2][9];
-                        results[results.length - 1][10] = results[results.length - 2][10];
-                        results[results.length - 1][11] = results[results.length - 2][11];
-                        results[results.length - 1][12] = results[results.length - 2][12];
-                        results[results.length - 1][13] = Double.parseDouble(results[results.length - 2][13].toString())+(maxTime - Double.parseDouble(results[results.length - 2][1].toString())) * Double.parseDouble(results[results.length - 2][3].toString());
-                        results[results.length - 1][14] = results[results.length - 2][14];
-                        results[results.length - 1][15] = Double.parseDouble(results[results.length - 2][15].toString())+(maxTime - Double.parseDouble(results[results.length - 2][1].toString())) * Double.parseDouble(results[results.length - 2][4].toString());
-
+                    } catch (NullPointerException ex) {
+                        System.out.println(ex);
+                        ms.generateNewEntity();
+                        continue;
+                    } catch (Exception exception) {
+                        System.out.println(exception);
+                        break;
                     }
-
-                    double qtSum = computeAreaUnderCurve(results, 'q');
-                    double btSum = computeAreaUnderCurve(results, 'b');
-
-                    reformatTable();
-                    // Updating the table with the new results
-                    table.setModel(new DefaultTableModel(
-                            results,
-                            headers
-                    ));
-
-                    // Updating the analytics panel with the new values
-                    nppValue.setText((results[results.length - 1][7])+ " ") ;
-                    ltsValue.setText((results[results.length - 1][10]) + " mins");
-                    twtValue.setText(String.valueOf(results[results.length - 1][9]));
-                    double awt =  ((Number)results[results.length - 1][9]).doubleValue() / ((Number)results[results.length - 1][8]).doubleValue();
-                    awtValue.setText( String.format("%.2f", awt) + " mins/part");
-                    nptValue.setText(String.valueOf(results[results.length - 1][8]));
-                    ltValue.setText(String.valueOf(results[results.length - 1][12]));
-                    double ats = (((Number)results[results.length - 1][11]).doubleValue() / ((Number)results[results.length - 1][7]).doubleValue());
-                    atsValue.setText( String.format("%.2f", ats)+ " mins/part");
-                    auqvalue.setText(String.format("%.2f",qtSum));
-                    htqValue.setText(getMax(results));
-                    aqtValue.setText(String.format("%.2f", (qtSum / maxTime)) + " part"); //
-                    ausValue.setText(String.valueOf(btSum));
-                    udpValue.setText(String.format("%.2f", (btSum/maxTime * 100)) + "%"); //
+                }
+                //put the first element at the end
+                ArrayList<Object> firstElement = rows.remove(0);
+                rows.add(firstElement);
+                results = rows.stream().map(u -> u.toArray(new Object[0])).toArray(Object[][]::new);
+                if (((Number) results[results.length - 1][1]).doubleValue() > maxTime) {
+                    results[results.length - 1][0] = " ";
+                    results[results.length - 1][1] = maxTime;
+                    results[results.length - 1][2] = "End";
+                    results[results.length - 1][3] = results[results.length - 2][3];
+                    results[results.length - 1][4] = results[results.length - 2][4];
+                    results[results.length - 1][5] = results[results.length - 2][5];
+                    results[results.length - 1][6] = results[results.length - 2][6];
+                    results[results.length - 1][7] = results[results.length - 2][7];
+                    results[results.length - 1][8] = results[results.length - 2][8];
+                    results[results.length - 1][9] = results[results.length - 2][9];
+                    results[results.length - 1][10] = results[results.length - 2][10];
+                    results[results.length - 1][11] = results[results.length - 2][11];
+                    results[results.length - 1][12] = results[results.length - 2][12];
+                    results[results.length - 1][13] = Double.parseDouble(results[results.length - 2][13].toString()) + (maxTime - Double.parseDouble(results[results.length - 2][1].toString())) * Double.parseDouble(results[results.length - 2][3].toString());
+                    results[results.length - 1][14] = results[results.length - 2][14];
+                    results[results.length - 1][15] = Double.parseDouble(results[results.length - 2][15].toString()) + (maxTime - Double.parseDouble(results[results.length - 2][1].toString())) * Double.parseDouble(results[results.length - 2][4].toString());
                 }
 
+                double qtSum = computeAreaUnderCurve(results, 'q');
+                double btSum = computeAreaUnderCurve(results, 'b');
+
+                reformatTable();// Updating the table with the new results
+                table.setModel(new DefaultTableModel(
+                        results,
+                        headers
+                ));
+
+                // Updating the analytics panel with the new values
+                ltsValue.setText((results[results.length - 1][10]) + " mins");
+                nppValue.setText((results[results.length - 1][7]) + " ");
+                twtValue.setText(String.valueOf(results[results.length - 1][9]));
+                double awt = ((Number) results[results.length - 1][9]).doubleValue() / ((Number) results[results.length - 1][8]).doubleValue();
+                awtValue.setText(String.format("%.2f", awt) + " mins/part");
+                nptValue.setText(String.valueOf(results[results.length - 1][8]));
+                ltValue.setText(String.valueOf(results[results.length - 1][12]));
+                double ats = (((Number) results[results.length - 1][11]).doubleValue() / ((Number) results[results.length - 1][7]).doubleValue());
+                atsValue.setText(String.format("%.2f", ats) + " mins/part");
+                auqvalue.setText(String.format("%.2f", qtSum));
+                htqValue.setText(getMax(results));
+                aqtValue.setText(String.format("%.2f", (qtSum / maxTime)) + " part"); //
+                ausValue.setText(String.valueOf(String.format("%.2f", btSum)));
+                udpValue.setText(String.format("%.2f", (btSum / maxTime * 100)) + "%"); //
             }
-        }
+
+
+                                         }
         );
     }
 
+    // Start the queuing simulation
+    public static void main(String[] args) {
+        ManufacturingSystemGUI qs = new ManufacturingSystemGUI();
+    }
 
-    public double computeAreaUnderCurve(Object[][] table, char cat){
+    public double computeAreaUnderCurve(Object[][] table, char cat) {
         double sum = 0;
-        for(int i = 2; i <table.length;i++){
-            if(cat == 'q'){
-                sum += (((Number) table[i][1]).doubleValue() - ((Number) table[i-1][1]).doubleValue()) * ((Number) table[i-1][3]).doubleValue() ;
+        for (int i = 2; i < table.length; i++) {
+            if (cat == 'q') {
+                sum += (((Number) table[i][1]).doubleValue() - ((Number) table[i - 1][1]).doubleValue()) * ((Number) table[i - 1][3]).doubleValue();
             }
 
-            if(cat == 'b'){
-                sum += (((Number) table[i][1]).doubleValue() - ((Number) table[i-1][1]).doubleValue()) * ((Number) table[i-1][4]).doubleValue() ;
+            if (cat == 'b') {
+                sum += (((Number) table[i][1]).doubleValue() - ((Number) table[i - 1][1]).doubleValue()) * ((Number) table[i - 1][4]).doubleValue();
             }
         }
         return sum;
     }
 
-    public String getMax(Object[][] table){
+    public String getMax(Object[][] table) {
         int max = 0;
-        for(int i = 0; i <table.length;i++){
+        for (int i = 0; i < table.length; i++) {
             max = Math.max(max, (Integer) table[i][3]);
         }
         return String.valueOf(max);
     }
 
-    public void reformatTable(){
-        for(int i = 0; i < results.length; i++){
-            if(Double.parseDouble(results[i][6].toString())  == -1){
+    public void reformatTable() {
+        for (int i = 0; i < results.length; i++) {
+            if (Double.parseDouble(results[i][6].toString()) == -1) {
                 results[i][6] = " - ";
             }
         }
-    }
-    // Start the queuing simulation
-    public static void main(String[] args) {
-        ManufacturingSystemGUI qs = new ManufacturingSystemGUI();
     }
 
 }
